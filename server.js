65852e53bda2af6be162dae2a6f33968650ecd7d
Nandrianina <dodisoa.nandrianina@gmail.com>
2026-03-25 12:23:40 +0100

    fix: CORS + https src URL

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // --- MIDDLEWARE ---
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://moziik.netlify.app'],
+  origin: ['*'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
